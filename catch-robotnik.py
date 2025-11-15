c1c7bb25ff5d06ee698b6027f5aa392762d2274c
KeyButton100 <darren.svp@gmail.com>
2025-11-15 14:26:06 +0700

    Add files via upload
    new ver

--- catch-robotnik.py
+++ catch-robotnik.py
@@ -4,21 +4,36 @@ TITLE="Catch Dr. Robotnik!"
 Robotnik=Actor("eggman.png")
 Robotnik.pos=random.randint(0, 800), random.randint(0, 500)
 score=0
+KTH=Actor("knux.png")
 message="Catch the Doctor!"
+game=False
 def draw():
     screen.fill("Blue")
     Robotnik.draw()
+    KTH.draw()
     screen.draw.text(message, (10, 10))
     screen.draw.text("score="+str(score), (10, 30))
 
 def on_mouse_down(pos):
     global score, message
-    print(pos)
-    if Robotnik.collidepoint(pos):
-      message="Caught!"
-      score+=10
-    else:
-       message="Oops!"
-       score-=5
-    Robotnik.pos=random.randint(0, 800), random.randint(0, 500)
+    #print(pos)
+    if not game:
+      if KTH.collidepoint(pos):
+        message="Wrong person!"
+        score-=10
+      if Robotnik.collidepoint(pos):
+        message="Caught!"
+        score+=10
+      else:
+        message="Oops!"
+        score-=5
+      Robotnik.pos=random.randint(0, 800), random.randint(0, 500)
+      KTH.pos=random.randint(0,800), random.randint(0,500)
+def gameover():
+   global game, message
+   game=True
+   message="Game Over"
+clock.schedule(gameover, 30.0)
+
+
 pgzrun.go()
